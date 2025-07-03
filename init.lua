1cae39829ecd2d36a90c97fa54ba000b8ae9d212
Tibbitin <moshidamc@gmail.com>
2025-07-03 17:38:15 -0400

    Added more sets and keybinds

--- init.lua
+++ init.lua
@@ -84,7 +84,16 @@ I hope you enjoy your Neovim journey,
 P.S. You can delete this when you're done too. It's your config now! :)
 --]]
 
---local harpoon = require 'harpoon'
+-- MY SETS
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.scrolloff = 8
 
 -- Set <space> as the leader key
 -- See `:help mapleader`
@@ -203,6 +212,14 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 
 -- MY KEYMAPS
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+
+vim.keymap.set('v', '<S-Up>', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', '<S-Down>', ":m '<-2<CR>gv=gv")
+
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+vim.keymap.set('n', 'n', 'nzzzv')
+vim.keymap.set('n', 'N', 'Nzzzv')
 
 -- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
 -- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
